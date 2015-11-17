268dd8ff48033a022958a3833cd4b85d23603b42
Sturla <sturla22@gmail.com>
2015-11-17 02:01:20 +0000

    add sockets and calls to python

--- backend.js
+++ backend.js
@@ -7,14 +7,14 @@ var client = redis.createClient(6379, 'localhost', {no_ready_check: true});
 client.auth('password', function (err) {
     if (err) throw err;
 });
-
+var state = {};
 client.on('connect', function() {
     console.log('Connected to Redis');
     client.get("state",function(err,data){
 		if(!data){
 			console.log("Could not get data from Redis")
 		}else{
-			state = JSON.parse( data.toString() );
+			var state = JSON.parse( data.toString() );
 		}
 	});
 });
@@ -23,4 +23,61 @@ var app = express();
 var server = http.createServer(app);
 var serv_io = io.listen(server);
 server.listen(8080);
-app.use(express.static('.'))
+app.use(express.static('./bin/'));
+function updateState(data){
+	state = data;
+	state.getColor = function(){
+		if(this.groups[this.currentGroup].white){
+			return -1;
+		}else{
+			return this.groups[this.currentGroup].color;
+		}
+	}
+	state.getBrightness = function(){
+		var group = this.groups[this.currentGroup]
+		if(group.white){
+			return group.whiteBrightness;
+		}else{
+			return group.colorBrightness;
+		}
+	}
+	state.isOn = function(){
+		return this.groups[this.currentGroup].on;
+	}
+	state.isWhite = function(){
+		return this.groups[this.currentGroup].white;
+	}
+	client.set("state",JSON.stringify(state));
+}
+serv_io.sockets.on('connection',function(socket){
+	// socket.emit('update',state)
+	socket.on('group',function(data){
+		updateState(data);
+		exec("python /home/pi/Milight/wrapper.py " + state.currentGroup + " ON");
+	});
+	socket.on('white',function(data){
+		updateState(data);
+		if(state.isWhite()){
+			exec("python /home/pi/Milight/wrapper.py " + state.currentGroup + " ON -w");
+		}else{
+			exec("python /home/pi/Milight/wrapper.py " + state.currentGroup + " ON -c" + state.getColor());
+		}
+	});
+	socket.on('brightness',function(data){
+		updateState(data);
+		exec("python /home/pi/Milight/wrapper.py " + state.currentGroup + " ON -b " + state.getBrightness());
+	});
+	socket.on('color',function(data){
+		updateState(data);
+		exec("python /home/pi/Milight/wrapper.py " + state.currentGroup + " ON -c " + state.getColor());
+	});
+	socket.on('power',function(data){
+		updateState(data);
+		if(state.isOn()){
+			exec("python /home/pi/Milight/wrapper.py " + state.currentGroup + " ON");
+		}else{
+			exec("python /home/pi/Milight/wrapper.py " + state.currentGroup + " OFF");
+		}
+
+	});
+});
--- bin/frontend.js
+++ bin/frontend.js
@@ -1,103 +1,131 @@
 var state = new state();
-state.update(1,1,88,1,25,25);
-state.update(2,1,88,0,15,25);
-state.update(3,1,88,0,25,15);
-state.update(4,1,155,0,25,25);
-$(function(){
-	function adjustColor(color){
-		var ADJ = 30;
-		if(color - ADJ < 360){
-			return color + (180 + ADJ);
-		}else{
-			return color - ADJ;
-		}
-	}
-	function refreshContent(){
-		$("body").css("background-color",function(){
-			var s = 50;
-			if(state.isWhite()){s = 0;}
-			var h = adjustColor(parseInt(state.getColor() * (360 / 255)));
-			var l = 5;
-			if(state.isOn()){
-				l = state.getBrightness()*(50/25)+10;
-			}else{
-				s = 0;
-			}
-			return "hsl("+h+","+s+"%,"+l+"%)";
-		});
-		$("#info").html(state.groups[state.currentGroup].toString());
-	}
-	function btnClick (button) {
-		$("#b"+button).click(function(){
-			state.currentGroup = button;
-			refreshContent();
-	        $(".groupBtn").removeClass('btn-success').addClass('btn-default');
-	        $("#b"+button).removeClass('btn-default').addClass('btn-success');
-	        if(state.isWhite()){
-	        	$("#white").removeClass('btn-default').addClass('btn-success');
-	        }else{
-	        	$("#white").removeClass('btn-success').addClass('btn-default');
-	        }
-	        if (state.isOn()) {
-	        	$("#pwr").removeClass('btn-default').addClass('btn-success');
-	        }else{
-	        	$("#pwr").removeClass('btn-success').addClass('btn-default');
-	        }
-	        $("#color").slider("value",state.getColor());
-	        $("#brightness").slider("value",state.getBrightness());
-	        // send serial command and update db
-		});
-	}
-	for (var i = 0; i < 5; i++) {
-		btnClick(i);
-	};
+var socket = io.connect();
 
-	$("#white").click(function(){
-		$(this).toggleClass('btn-default').toggleClass('btn-success')
-		state.toggleWhite();
-		refreshContent();
-	    $("#brightness").slider("value",state.getBrightness());
-	    // send serial command and update db
-	});
-	$("#pwr").click(function(){
-		$(this).toggleClass('btn-default').toggleClass('btn-success')
-		state.togglePower();
-		refreshContent();
-		// send serial command and update db
-	});
 
-	$("#brightness").slider({
-		min:0,
-		max:25,
-		change:function(evt,ui){
-			if(evt.originalEvent){
-				state.setBrightness(ui.value);
-				refreshContent();
-				// send serial command and update db
-			}
-		},
-		slide:function(evt,ui){
-			if(evt.originalEvent){
-				state.setBrightness(ui.value);
-				refreshContent();
-			}
-		}
-	})
-	$("#color").slider({
-		min:0,
-		max:255,
-		change:function(evt,ui){
-			if(evt.originalEvent){
-				state.setBrightness(ui.value);
-				refreshContent();
-				// send serial command and update db
-			}
-		},
-		slide:function(evt,ui){
-			if(evt.originalEvent){
-				state.setBrightness(ui.value);
-				refreshContent();
-			}
-		}
-	})
-})
+$(function() {
+    function adjustColor(color) {
+        var ADJ = 30;
+        if (color - ADJ < 360) {
+            return color + (180 + ADJ);
+        } else {
+            return color - ADJ;
+        }
+    }
+
+    function refreshContent() {
+        $("body").css("background-color", function() {
+            var s = 50;
+            if (state.isWhite()) {
+                s = 0;
+            }
+            var h = adjustColor(parseInt(state.getColor() * (360 / 255)));
+            var l = 5;
+            if (state.isOn()) {
+                l = state.getBrightness() * (50 / 25) + 10;
+            } else {
+                s = 0;
+            }
+            return "hsl(" + h + "," + s + "%," + l + "%)";
+        });
+        $("#info").html(state.groups[state.currentGroup].toString());
+    }
+
+    function btnClick(button) {
+        $("#b" + button).click(function() {
+            state.currentGroup = button;
+            refreshContent();
+            $(".groupBtn").removeClass('btn-success').addClass('btn-default');
+            $("#b" + button).removeClass('btn-default').addClass('btn-success');
+            if (state.isWhite()) {
+                $("#white").removeClass('btn-default').addClass('btn-success');
+            } else {
+                $("#white").removeClass('btn-success').addClass('btn-default');
+            }
+            if (state.isOn()) {
+                $("#pwr").removeClass('btn-default').addClass('btn-success');
+            } else {
+                $("#pwr").removeClass('btn-success').addClass('btn-default');
+            }
+            $("#color").slider("value", state.getColor());
+            $("#brightness").slider("value", state.getBrightness());
+            // send serial command and update db
+            socket.emit('group', state);
+        });
+    }
+    for (var i = 0; i < 5; i++) {
+        btnClick(i);
+    };
+
+    $("#white").click(function() {
+        $(this).toggleClass('btn-default').toggleClass('btn-success')
+        state.toggleWhite();
+        refreshContent();
+        $("#brightness").slider("value", state.getBrightness());
+        // send serial command and update db
+        socket.emit('white', state);
+    });
+    $("#pwr").click(function() {
+        $(this).toggleClass('btn-default').toggleClass('btn-success')
+        state.togglePower();
+        refreshContent();
+        // send serial command and update db
+        socket.emit('power', state);
+    });
+
+    $("#brightness").slider({
+        min: 0,
+        max: 25,
+        change: function(evt, ui) {
+            if (evt.originalEvent) {
+                state.setBrightness(ui.value);
+                refreshContent();
+                // send serial command and update db
+                socket.emit('brightness', state);
+            }
+        },
+        slide: function(evt, ui) {
+            if (evt.originalEvent) {
+                state.setBrightness(ui.value);
+                refreshContent();
+            }
+        }
+    });
+    $("#color").slider({
+        min: 0,
+        max: 255,
+        change: function(evt, ui) {
+            if (evt.originalEvent) {
+                state.setColor(ui.value);
+                refreshContent();
+                // send serial command and update db
+                socket.emit('color', state);
+            }
+        },
+        slide: function(evt, ui) {
+            if (evt.originalEvent) {
+                state.setColor(ui.value);
+                refreshContent();
+            }
+        }
+    });
+    socket.on('update', function(data) {
+        state = data;
+        $(".groupBtn").removeClass('btn-success').addClass('btn-default');
+        $("#b" + state.currentGroup).removeClass('btn-default').addClass('btn-success');
+        if (state.isWhite()) {
+            $("#white").removeClass('btn-default').addClass('btn-success');
+        } else {
+            $("#white").removeClass('btn-success').addClass('btn-default');
+        }
+        if (state.isOn()) {
+            $("#pwr").removeClass('btn-default').addClass('btn-success');
+        } else {
+            $("#pwr").removeClass('btn-success').addClass('btn-default');
+        }
+        $("#color").slider("value", state.getColor());
+        $("#brightness").slider("value", state.getBrightness());
+    });
+});
+$(window).on('beforeunload', function() {
+    socket.close();
+});
